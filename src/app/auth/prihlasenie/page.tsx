7a91c3b57a03e367b764f3272599d24265ee7eac
666pesi <samuellegat@s.zochova.sk>
2024-10-24 09:02:14 +0200

    update

--- src/app/auth/prihlasenie/page.tsx
+++ src/app/auth/prihlasenie/page.tsx
@@ -1,7 +1,6 @@
 // src/app/auth/prihlasenie/page.tsx
 
 import AuthHomeView from "@/sections/AuthHomeView";
-import SignInView from "@/sections/AuthHomeView";
 
 export const metadata = { title: "Prihlásenie užívateľa | ZoškaSnap" };
 
